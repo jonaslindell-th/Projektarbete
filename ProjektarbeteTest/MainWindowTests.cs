88066954fa2d6ccdfbdca0a23d590356dd099a47
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2020-11-08 03:04:21 +0100

    Added test class

---
+++ ProjektarbeteTest/MainWindowTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Projektarbete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace ProjektarbeteTest
+{
+    [TestClass()]
+    public class MainWindowTests
+    {
+        [TestMethod()]
+        public void InvalidCouponTest()
+        {
+            Assert.AreEqual(false, Coupon.IsValid("invalidCode"));
+        }
+
+        public void SaveCartTest()
+        {
+
+        }
+    }
+}
