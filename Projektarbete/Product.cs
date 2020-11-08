047a6b470729c282fe77a32c6a2135ec3c48d04f
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2020-11-08 03:02:07 +0100

    Made product class public for testing

--- Projektarbete/Product.cs
+++ Projektarbete/Product.cs
@@ -6,27 +6,30 @@ using System.Text.Json;
 using System.Reflection;
 using System.Linq;
 
-class Product
+namespace Projektarbete
 {
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public decimal Price { get; set; }
-    public string ProductImage { get; set; }
-    public int Count { get; set; }
-    public string Category { get; set; }
-
-    public static List<Product> DeserializeProducts(string path)
+    public class Product
     {
-        if (!File.Exists(path)) File.Create(path).Close();
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ProductImage { get; set; }
+        public int Count { get; set; }
+        public string Category { get; set; }
 
-        List<Product> items = new List<Product>();
-        using (StreamReader reader = new StreamReader(path))
+        public static List<Product> DeserializeProducts(string path)
         {
-            var products = reader.ReadToEnd();
+            if (!File.Exists(path)) File.Create(path).Close();
 
-            items = JsonSerializer.Deserialize<List<Product>>(products);
-            reader.Close();
+            List<Product> items = new List<Product>();
+            using (StreamReader reader = new StreamReader(path))
+            {
+                var products = reader.ReadToEnd();
+
+                items = JsonSerializer.Deserialize<List<Product>>(products);
+                reader.Close();
+            }
+            return items;
         }
-        return items;
     }
 }
