e4c094a8e65aa9eafbe851e6019538f9c42eff3e
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2020-11-08 03:24:05 +0100

    Removed GenerateCoupon method since we won't use it for anything

--- Projektarbete/Coupon.cs
+++ Projektarbete/Coupon.cs
@@ -2,11 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace Projektarbete
 {
@@ -19,27 +15,6 @@ namespace Projektarbete
         public static bool IsValid(string code)
         {
             return CouponCodes().Any(x => x.Code == code);
-        }
-
-        //We'll use this to generate a coupon for the user (After they input the discount percentage)
-        public static Coupon GenerateCoupon(int discount)
-        {
-            string code = string.Empty;
-            string chars = "ABCDEFGHIJKLMNOPQRSTUVXYZ123456789";
-            Random r = new Random();
-
-            //generate an uppercase alphanumeric code of length 10 that'll be useable as a coupon code
-            for(int i = 0; i < 10; i++)
-            {
-                int random = r.Next(0, chars.Length - 1);
-                code += chars[random];
-            }
-
-            return new Coupon()
-            {
-                Code = code,
-                Discount = 1 - ((decimal)discount / 100) //discount is an int so the user can input "70" for 70% (more user friendly than inputting 0.7)
-            };
         }
 
         public static List<Coupon> CouponCodes()
